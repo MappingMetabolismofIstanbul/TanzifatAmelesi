8e4a9c4c047423efee8aa8b2a7b023dcd932caf7
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-08-09 00:44:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'Tanzifat Amelesi / Street Cleaner',
+    title: 'Tanzifat Amelesi / Ottoman Street Sweeper',
     subtitle: '',
     byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
     footer: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
